2cfa0c13e0da1b3bc95635aa4aa40ee3753c2dd7
Taylor Osbourne <taylorgosbourne@gmail.com>
2019-12-19 10:50:40 -0500

    added card comparison to home page

--- src/containers/Layout.js
+++ src/containers/Layout.js
@@ -2,12 +2,10 @@ import React from 'react';
 import { Link } from "react-router-dom";
 import Nav from "../components/Nav";
 import Footer from './Footer';
-// import Header from '../containers/Header';
 
 const Layout = ({ children }) => (
   <>
     <Nav/>
-    {/* <Header /> */}
     <main
       style={{
         width: `80%`,
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,20 +1,22 @@
 import React from 'react';
 import styled from 'styled-components';
+import TimeSpiral from '../assets/time-spiral.jpg';
+import TimeReversal from '../assets/time-reversal.jpg';
 
 const StyledFormInput = styled.input`
-  display: block; 
+  display: block;
   margin-bottom: 25px;
-  font-size: 1.25rem; 
+  font-size: 1.25rem;
   width: 100%;
-  border: 0; 
-  border-bottom: 1px solid rgb(142,142,142);
+  border: 0;
+  border-bottom: 1px solid rgb(142, 142, 142);
   :focus {
     outline: none;
-    border-bottom: 1.5px solid #00CC7B;
+    border-bottom: 1.5px solid #00cc7b;
   }
 `;
 
-const SearchContainer = styled.div`
+const SearchContainer = styled.section`
   background: #fff;
   width: 100%;
   height: 500px;
@@ -23,9 +25,12 @@ const SearchContainer = styled.div`
     max-width: 100%;
     height: 20px;
     padding: 15px;
-    border-bottom: 8px solid rgb(242,242,242);
+    border-bottom: 8px solid rgb(242, 242, 242);
     font-weight: 200;
     text-transform: uppercase;
+    @media(max-width: 700px) {
+      font-size: 1.25rem;
+    }
   }
 `;
 
@@ -39,11 +44,14 @@ const BottomCardsContainer = styled.div`
     max-width: 100%;
     height: 20px;
     padding: 0px 5px 15px 15px;
-    border-bottom: 8px solid rgb(242,242,242);
+    border-bottom: 8px solid rgb(242, 242, 242);
     font-weight: 200;
     text-transform: uppercase;
+    @media(max-width: 700px) {
+      font-size: 1.25rem;
+    }
   }
-  @media(max-width: 955px) {
+  @media (max-width: 955px) {
     flex-direction: column;
     * {
       margin-bottom: 35px;
@@ -51,7 +59,7 @@ const BottomCardsContainer = styled.div`
   }
 `;
 
-const RecentCards = styled.div`
+const RecentCards = styled.section`
   width: 48.5%;
   height: 600px;
   background: #fff;
@@ -59,53 +67,189 @@ const RecentCards = styled.div`
     max-width: 100%;
     height: 20px;
     padding: 0px 5px 15px 15px;
-    border-bottom: 8px solid rgb(242,242,242);
+    border-bottom: 8px solid rgb(242, 242, 242);
     font-weight: 200;
     text-transform: uppercase;
+    @media(max-width: 700px) {
+      font-size: 1.25rem;
+    }
   }
-  @media(max-width: 955px) {
+  @media (max-width: 955px) {
     width: 100%;
   }
 `;
 
-const SignUpContainer = styled.div`
+const SignUpContainer = styled.section`
   width: 48.5%;
   height: 500px;
   background: #fff;
-  @media(max-width: 955px) {
+  @media (max-width: 955px) {
     width: 100%;
     height: 550px;
   }
 `;
 
-const Home = () => {
-  return (
-    <>
-      <SearchContainer>
-        <h2>Find the best cards at the best <span style={{color: `#00CC7B`}}>price&#36;</span></h2>
-      </SearchContainer>
-      <BottomCardsContainer>
-        <RecentCards>
-          <h2>Recent <span style={{color: `#00CC7B`}}>budget</span> alternates</h2>
-        </RecentCards>
-        <SignUpContainer>
-            <h2><span style={{color: `#00CC7B`}}>Sign up</span> to contribute</h2>
-            <form style={{padding: `22px`}}>
-              <label for="email">Email</label>
-              <StyledFormInput name="email" type="email" />
-              <label for="confirmEmail">Confirm Email</label>
-              <StyledFormInput name="confirmEmail" type="email" />
-              <label for="password">Password</label>
-              <StyledFormInput name="password" type="password" />
-              <label for="confirmPassword">Confirm Password</label>
-              <StyledFormInput name="confirmPassword" type="password" />
-              <button style={{marginBottom: `25px`, fontSize: `1.5rem`, background: `#00CC7B`, color: `#fff`, padding: `10px 20px`, border: 0, cursor: `pointer`}}>Sign Up</button>
-              <a style={{textDecoration: `none`, color: `#00CC7B`, display: `block`, cursor: `pointer`}}>Already have an account?  Log in</a>
-            </form>
-          </SignUpContainer>
-       </BottomCardsContainer>
-    </>
-  );
-};
+const CardsContentContainer = styled.div`
+  width: 94%;
+  height: 80%;
+  margin: 0 auto;
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-start;
+  @media(max-width: 1000px){
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
+  }
+`;
 
+const CardPriceContainer = styled.div`
+  :hover {
+    span {
+      opacity: 1;
+    }
+  }
+`;
+
+const ExpensiveCard = styled.img`
+  height: 90%;
+  width: auto;
+  position: absolute;
+  top: 10px;
+  left: 50px;
+  border-radius: 15px;
+  transition: 0.3s ease;
+  :hover {
+    transform: translateX(-15px);
+  }
+`;
+
+const ExpensiveCardPrice = styled.span`
+  display: inline-block;
+  position: absolute;
+  background: rgba(0,0,0,0.8);
+  padding: 10px 20px;
+  color: tomato;
+  opacity: 0;
+  transition: 0.3s ease;
+`;
+
+const CheapCardPrice = styled.span`
+  display: inline-block;
+  position: absolute;
+  right: 35px;
+  top: 25px;
+  background: rgba(0,0,0,0.8);
+  padding: 10px 20px;
+  color: #00cc7b;
+  opacity: 0;
+  transition: 0.3s ease;
+`;
+
+const CheapCard = styled.img`
+  height: 90%;
+  width: auto; 
+  position: absolute;
+  top: 35px;
+  right: 100px;
+  border-radius: 15px;
+   transition: 0.3s ease;
+  :hover {
+    transform: translateX(15px);
+  }
+`;
+
+const Home = () => {
+  console.log(window.innerWidth)
+return (
+  <>
+    <SearchContainer>
+      <h2>
+        <span style={{ color: `#00CC7B` }}>search</span> budget alternates
+      </h2>
+        <CardsContentContainer>
+          <form style={window.innerWidth < `1000` ? {} : {width: `50%`}}>
+          {/* <label for="cardName">Search Card</label> */}
+          <StyledFormInput type="text" style={{fontSize: `2.5rem`, fontStyle: `italic`, width: `100%`, margin: `15px 0`}} placeholder="cyclonic rift..." />
+           <button
+            style={{
+              marginBottom: `25px`,
+              fontSize: `1.5rem`,
+              background: `transparent`,
+              color: `#00CC7B`,
+              padding: `10px 20px`,
+              border: `1px solid #00CC7B`,
+              cursor: `pointer`
+            }}
+          >
+            Search
+          </button>
+          </form>
+          <div 
+            style={{
+              width: `45%`, 
+              height: `100%`, 
+              position: `relative`,
+              // background: `tomato`
+            }}
+          >
+            <CardPriceContainer>
+              <ExpensiveCard src={TimeSpiral} alt="expensive-card" />
+              <ExpensiveCardPrice>YIKES &#36;63</ExpensiveCardPrice>
+            </CardPriceContainer>
+            <CardPriceContainer>
+              <CheapCard src={TimeReversal} alt="cheap-card" />
+              <CheapCardPrice>NICE &#36;3</CheapCardPrice>
+            </CardPriceContainer>
+          </div>
+        </CardsContentContainer>
+    </SearchContainer>
+    <BottomCardsContainer>
+      <RecentCards>
+        <h2>
+          Recent <span style={{ color: `#00CC7B` }}>budget</span> alternates
+        </h2>
+      </RecentCards>
+      <SignUpContainer>
+        <h2>
+          <span style={{ color: `#00CC7B` }}>Sign up</span> to contribute
+        </h2>
+        <form style={{ padding: `22px` }}>
+          <label for="email">Email</label>
+          <StyledFormInput name="email" type="email" />
+          <label for="confirmEmail">Confirm Email</label>
+          <StyledFormInput name="confirmEmail" type="email" />
+          <label for="password">Password</label>
+          <StyledFormInput name="password" type="password" />
+          <label for="confirmPassword">Confirm Password</label>
+          <StyledFormInput name="confirmPassword" type="password" />
+          <button
+            style={{
+              marginBottom: `25px`,
+              fontSize: `1.5rem`,
+              background: `#00CC7B`,
+              color: `#fff`,
+              padding: `10px 20px`,
+              border: 0,
+              cursor: `pointer`
+            }}
+          >
+            Sign Up
+          </button>
+          <a
+            style={{
+              textDecoration: `none`,
+              color: `#00CC7B`,
+              display: `block`,
+              cursor: `pointer`
+            }}
+          >
+            Already have an account? Log in
+          </a>
+        </form>
+      </SignUpContainer>
+    </BottomCardsContainer>
+  </>
+);
+}
 export default Home;
